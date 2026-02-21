d9c6cdb421d571d50e1c246bfc5f09970d6d5596
ChatterMonkey <basu.maya@icloud.com>
2026-02-20 22:41:25 -0800

    removed endedness file check

--- python_plotting/fits.py
+++ python_plotting/fits.py
@@ -1,30 +1,18 @@
 import sys
-
 from astropy.io import fits
 import matplotlib.pyplot as plt
 import numpy as np
 
 file = fits.open("/Users/mayabasu/RustroverProjects/image_simulator_outline/python_plotting/UVEX_FUV_PSF_1um_F001 1.fits")
-
-
-data= file[0].data
-print(file[0].header)
-
+data = file[0].data
+print(data)
 plt.imshow(data)
+plt.show()
 
 np.set_printoptions(threshold=sys.maxsize)
-print(data)
-
 data = np.array(data).flatten()
+
+print(file[0].header)
 print(np.argmax(data))
 print(data[1955])
 
-
-
-with open("fits_data", "wb") as binary_file:
-    for pixel in data:
-        data_bytes = pixel.tobytes()
-        binary_file.write(data_bytes)
-
-
-plt.show()
